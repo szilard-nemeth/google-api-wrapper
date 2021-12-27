5c295cd3f5ed61031aaeb1626c157c9f19617618
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-12-27 14:58:15 +0100

    Specify gspread version in install_requires as well

--- setup.py
+++ setup.py
@@ -5,26 +5,21 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='google-api-wrapper',
-    version='0.1.0',
-    description='',
+    name="google-api-wrapper",
+    version="0.1.0",
+    description="",
     long_description=readme,
-    author='Szilard Nemeth',
-    author_email='szilard.nemeth88@gmail.com',
-    url='',
+    author="Szilard Nemeth",
+    author_email="szilard.nemeth88@gmail.com",
+    url="",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=[
-        'gspread',
-        'oauth2client',
-        'google-api-python-client==2.31.0'
-    ]
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=["gspread>=5.1.1", "oauth2client", "google-api-python-client==2.31.0"],
 )
-
